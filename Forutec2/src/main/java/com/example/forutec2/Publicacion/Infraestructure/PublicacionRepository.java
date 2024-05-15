2dfb9b9ade9a5d46249a95edceca33dd74658a51
egomezv19 <149917107+egomezv19@users.noreply.github.com>
2024-05-14 22:33:41 -0500

    Delete Forutec2 directory

--- Forutec2/src/main/java/com/example/forutec2/Publicacion/Infraestructure/PublicacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.forutec2.Publicacion.Infraestructure;
-
-import com.example.forutec2.Publicacion.Domain.Publicacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PublicacionRepository extends JpaRepository<Publicacion, Long> {
-}
--- Forutec2/src/main/java/com/example/forutec2/Usuario/Infraestructure/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.forutec2.Usuario.Infraestructure;
-
-import com.example.forutec2.Usuario.Domain.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
